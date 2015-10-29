83d0677dd85bd83821ad2c5d683a9df1f6d81edd
landongr <graham.cilandon@gmail.com>
2015-10-29 11:28:36 -0400

    Code Done

--- test_lab3.py
+++ test_lab3.py
@@ -13,7 +13,7 @@ MONTHS_WITH_31 = ["January", "March", "May", "July", "August", "October", "Decem
 MONTHS_WITH_30 = ["April", "June", "September", "November"]
 MONTHS_WITH_28_or_29 = ["February"]
 
-
+WEIRD_INPUT = ["Strange String" , ["list_item", "list_item2"], "another strange string", 5, [3,4,5]]
 def test_months_with_31():
     """
     Test months with 31 days
@@ -22,15 +22,37 @@ def test_months_with_31():
         assert days_in_month(item) == 31
 
 # Write a test function for the months with 30 days
-
+def test_months_with_30():
+    """
+    Test months with 30 days
+    """
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
 
 # Write a test function for the months with 28 or 29 days
-
+def test_months_with_28_or_29():
+    """
+    Test months with 28 or 29 days
+    """
+    for item in MONTHS_WITH_28_or_29:
+        assert days_in_month(item) == 28 or 29
 
 # Write a test function for months that are not capitalized properly
 # Hint: use the lower() string method
-
+def test_months_with_bad_names():
+    for item in MONTHS_WITH_31:
+        item = item.lower()
+        assert days_in_month(item) == 31
 # Write a test function for unexpected input
 # Hint: use a try/except block to deal with the exception
 # Hint: use data types other than strings as input
 
+def test_unexpected_input():
+    for item in WEIRD_INPUT:
+        try:
+            days_in_month(item)
+        except:
+            if ValueError:
+                assert True
+            if TypeError:
+                assert True
